e7c1ee0d16c8b6c8300e787efbbe75c0f4839a51
as093 <as0930859852@gmail.com>
2022-10-26 18:43:07 +0800

    2022_9_28_2

---
+++ src/p14.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class p14 {
+    public static void main(String args[]) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("輸入一字串:");
+        String str = scanner.nextLine();
+        System.out.println("總共有"+str.length()+"個字元");
+
+
+    }
+
+}
